7eaba199614f569eae3a06702aba222680d3c7d2
Cem Dervis <cem@dervis.de>
2024-08-30 16:42:37 +0200

    Patch dr_mp3.h to support Windows ARM

--- src/audiosource/wav/dr_mp3.h
+++ src/audiosource/wav/dr_mp3.h
@@ -2103,7 +2103,15 @@ static void drmp3d_synth(float *xl, drmp3d_sample_t *dstl, int nch, float *lins)
             vst1_lane_s16(dstl + (49 + i)*nch, pcmb, 2);
 #endif
 #else
-            static const drmp3_f4 g_scale = { 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f };
+
+#if defined(WIN32) && defined(_M_ARM64)
+          static const drmp3_f4 g_scale = {.n128_f32 = { 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f }};
+#elif defined(WIN32)
+          static const drmp3_f4 g_scale = {.m128_f32 = { 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f }};
+#else
+          static const drmp3_f4 g_scale = { 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f };
+#endif
+
             a = DRMP3_VMUL(a, g_scale);
             b = DRMP3_VMUL(b, g_scale);
 #if DRMP3_HAVE_SSE
